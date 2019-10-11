9ee1fe9b9575659f6c02c5614457d480e60426f9
Tory BYU <torys.anderson@gmail.com>
2019-10-11 12:13:48 -0600

    init

---
+++ About_files/script.js
@@ -0,0 +1,9 @@
+
+ jQuery(document).ready(function(){
+
+     jQuery(window).scroll(function(){
+
+        // jQuery(".page-id-151 #menu-fttv-menu-2 li.menu-item a").css("margin-top", Math.max(0, 250- jQuery(this).scrollTop()));
+ 	//	jQuery(".imageTest").css("opacity", 2 - jQuery(window).scrollTop() / (jQuery('.imageTest').height() / 2.5));
+     });
+ });
